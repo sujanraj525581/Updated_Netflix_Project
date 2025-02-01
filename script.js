ec353a89d90af8726cdc0a084e9fa782bd3a768c
Sujan raj <149222338+sujanraj525581@users.noreply.github.com>
2025-02-01 10:25:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+        window.onload = function () {
+            document.getElementById("myModal").style.display = "flex";
+        };
+
+        function closeModal() {
+            document.getElementById("myModal").style.display = "none";
+        }
